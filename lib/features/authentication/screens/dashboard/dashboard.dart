7a13b7658233f3f8011391344d9792fb1cf7d1e9
shreyxnsh <shreyanshatwork@gmail.com>
2023-11-09 20:30:35 +0530

    created dashboard screen

---
+++ lib/features/authentication/screens/dashboard/dashboard.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/common/styles/spacing_styles.dart';
+import 'package:t_store/features/authentication/screens/kyc/widgets/kyc_form.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/constants/text_strings.dart';
+
+import '../../../../utils/helpers/helper_functions.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(padding: TSpacingStyle.paddingWithAppBarHeight,
+        
+        child: Text("Dashboard")),
+      ),
+    );
+  }
+}
