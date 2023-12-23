4b842076d44fa0a9e4a74025d2c37a2bb06013de
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-24 00:14:57 +0530

    login screen ui fixes

--- lib/app.dart
+++ lib/app.dart
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: TTexts.appName,
-      themeMode: ThemeMode.light,
+      themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
