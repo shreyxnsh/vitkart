aaa8c0eecaba70d5249bdb89d99fa1beeddfc226
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-26 13:16:24 +0530

    MyTicket Screen completed

---
+++ lib/features/events/screens/myTickets.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/common/widgets/text/section_heading.dart';
+import 'package:t_store/features/events/screens/eventDetails.dart';
+import 'package:t_store/features/events/screens/widgets/eventTicketVertical.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+
+class MyTicketScreen extends StatelessWidget {
+  const MyTicketScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TAppBar(
+        title: Text(
+          "My Tickets",
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+            padding: const EdgeInsets.all(TSizes.defaultSpace),
+            child: Column(
+              children: [
+                const TSectionHeading(
+                  title: "Upcoming",
+                  textColor: TColors.primary,
+                  showActionButton: false,
+                ),
+                const SizedBox(
+                  height: TSizes.spaceBtwItems,
+                ),
+                EventTicketVertical(),
+                const SizedBox(
+                  height: TSizes.spaceBtwItems,
+                ),
+                EventTicketVertical(),
+                const SizedBox(
+                  height: TSizes.spaceBtwSections,
+                ),
+                const TSectionHeading(
+                  title: "Earlier",
+                 
+                  showActionButton: false,
+                ),
+                const SizedBox(
+                  height: TSizes.spaceBtwItems,
+                ),
+                EventTicketVertical(),
+                const SizedBox(
+                  height: TSizes.spaceBtwItems,
+                ),
+                EventTicketVertical(),
+              ],
+            )),
+      ),
+    );
+  }
+}
