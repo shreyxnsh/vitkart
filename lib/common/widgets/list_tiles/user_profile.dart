1909e137bbcc4f3f8378ba86aa28004a90758ebb
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-28 22:22:55 +0530

    Data fetched from token

--- lib/common/widgets/list_tiles/user_profile.dart
+++ lib/common/widgets/list_tiles/user_profile.dart
@@ -1,31 +1,60 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:vitkart/features/personalization/screens/profile/profile.dart';
+import 'package:vitkart/utils/API/userDataService.dart';
 
 import '../../../utils/constants/colors.dart';
 
 class TUserProfileTile extends StatelessWidget {
   const TUserProfileTile({
-    super.key, required this.onPressed,
+    super.key,
+    required this.onPressed,
   });
 
   final VoidCallback onPressed;
 
+
+
   @override
   Widget build(BuildContext context) {
+
+
+    // Define an empty userName
+    String userName = UserDataService.getUserName();
+    String userEmail = UserDataService.getUserEmail();
+
+
+    
+    
     return ListTile(
       leading: const CircleAvatar(
         radius: 25,
-        backgroundImage:
-            AssetImage('assets/images/content/user.png'),
+        backgroundImage: AssetImage('assets/images/content/user.png'),
       ),
-      title: Text("Shreyansh Jain", style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white,),),
-      subtitle: Text("shreyansh.jain2021@vitbhopal.ac.in", style: Theme.of(context).textTheme.bodyMedium!.apply(color: TColors.white,),),
-      trailing: IconButton(onPressed: (){
-        Get.to(()=> const ProfileScreen());
-      }, icon: const Icon(Iconsax.edit, color: TColors.white,),),
+      title: Text(
+        userName,
+        style: Theme.of(context).textTheme.headlineSmall!.apply(
+              color: TColors.white,
+            ),
+      ),
+      subtitle: Text(
+        userEmail,
+        style: Theme.of(context).textTheme.bodyMedium!.apply(
+              color: TColors.white,
+            ),
+      ),
+      trailing: IconButton(
+        onPressed: () {
+          Get.to(() => const ProfileScreen());
+        },
+        icon: const Icon(
+          Iconsax.edit,
+          color: TColors.white,
+        ),
+      ),
     );
   }
 }
