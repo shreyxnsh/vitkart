bb97cce4ea0ccd059171dad9c8a6bc00bdbc5f62
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-28 22:24:23 +0530

    globalized user token data - UserDataService

---
+++ lib/utils/API/userDataService.dart
@@ -0,0 +1,70 @@
+import 'package:get_storage/get_storage.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+
+class UserDataService {
+  
+  static String getUserName() {
+    final storedToken = GetStorage().read('token');
+    if (storedToken != null) {
+      Map<String, dynamic> jwtDecodedToken = JwtDecoder.decode(storedToken);
+      return jwtDecodedToken['userName'];
+    }
+    return '';
+  }
+
+  static String getUserEmail() {
+    final storedToken = GetStorage().read('token');
+    if (storedToken != null) {
+      Map<String, dynamic> jwtDecodedToken = JwtDecoder.decode(storedToken);
+      return jwtDecodedToken['userEmail'];
+    }
+    return '';
+  }
+
+  static String getUserID() {
+    final storedToken = GetStorage().read('token');
+    if (storedToken != null) {
+      Map<String, dynamic> jwtDecodedToken = JwtDecoder.decode(storedToken);
+      return jwtDecodedToken['userID'];
+    }
+    return '';
+  }
+
+  static String getUserBatch() {
+    final storedToken = GetStorage().read('token');
+    if (storedToken != null) {
+      Map<String, dynamic> jwtDecodedToken = JwtDecoder.decode(storedToken);
+      return jwtDecodedToken['userBatch'];
+    }
+    return '';
+  }
+
+  static String getUserBirthDate() {
+    final storedToken = GetStorage().read('token');
+    if (storedToken != null) {
+      Map<String, dynamic> jwtDecodedToken = JwtDecoder.decode(storedToken);
+      return jwtDecodedToken['userBirthDate'];
+    }
+    return '';
+  }
+
+  static String getUserRegID() {
+    final storedToken = GetStorage().read('token');
+    if (storedToken != null) {
+      Map<String, dynamic> jwtDecodedToken = JwtDecoder.decode(storedToken);
+      return jwtDecodedToken['userRegID'];
+    }
+    return '';
+  }
+
+  static String getUserGender() {
+    final storedToken = GetStorage().read('token');
+    if (storedToken != null) {
+      Map<String, dynamic> jwtDecodedToken = JwtDecoder.decode(storedToken);
+      return jwtDecodedToken['userGender'];
+    }
+    return '';
+  }
+
+  // Add more functions for additional user data fields as needed
+}
