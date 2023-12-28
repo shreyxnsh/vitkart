3bf3ccbba5a6ec166f9c74867cd63a8d602b3f1b
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-28 22:22:06 +0530

    GetStorage + JWTTokenDecode in Navigation

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:vitkart/features/events/screens/event.dart';
 import 'package:vitkart/features/personalization/screens/settings/settings.dart';
 import 'package:vitkart/features/shop/screens/home/home.dart';
@@ -8,13 +10,48 @@ import 'package:vitkart/features/shop/screens/store/store.dart';
 import 'package:vitkart/utils/constants/colors.dart';
 import 'package:vitkart/utils/helpers/helper_functions.dart';
 
-class NavigationMenu extends StatelessWidget {
-  const NavigationMenu({super.key});
+class NavigationMenu extends StatefulWidget {
 
+  final token;
+  const NavigationMenu({super.key, this.token});
+
+  @override
+  State<NavigationMenu> createState() => _NavigationMenuState();
+}
+
+class _NavigationMenuState extends State<NavigationMenu> {
+  
+  late String userId;
+  late String userName;
+  
+  @override
+  void initState() { 
+    super.initState();
+
+    final userToken = GetStorage().read('token');
+
+    if (widget.token != null) {
+    Map<String, dynamic> jwtDecodedToken = JwtDecoder.decode(userToken);
+
+    // getting the user id of the user from db by variable _id from tokenData
+
+    userId = jwtDecodedToken['userID'];
+    userName = jwtDecodedToken['userName'];
+    // userId = jwtDecodedToken['_id'];
+    print("User token in NavigationMenu is : ${widget.token}");
+    print("User ID in NavigationMenu is : $userId");
+    print("User Name in NavigationMenu is : $userName");
+  } else {
+    // Handle the case where the token is null
+    print("Token is null");
+  }
+  }
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(NavigationController());
     final darkMode = THelperFunctions.isDarkMode(context);
+
+  
 
     return Scaffold(
       bottomNavigationBar: Obx(
