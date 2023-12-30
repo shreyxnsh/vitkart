45412108b2a92228578fa0eab2ca6447b7f06e44
Aviral <yaviral17@gmail.com>
2023-12-30 07:05:43 +0530

    otp email,otp countdown,cherry toast for page 1,2,3

--- lib/features/authentication/controllers/register/register_controller.dart
+++ lib/features/authentication/controllers/register/register_controller.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
+import 'package:vitkart/features/authentication/screens/register/widget/cherryToast.dart';
 import 'package:vitkart/utils/config/config.dart';
 import 'package:vitkart/utils/constants/sizes.dart';
 
@@ -29,7 +30,8 @@ class RegisterController extends GetxController {
   TextEditingController otpController = TextEditingController();
   RxBool isVerified = false.obs;
   RxBool canResend = true.obs;
-
+  RxInt countdown = 30.obs;
+  RxInt otpResendCount = 0.obs;
   PageController pageController = PageController();
 
   RxBool isPassObscure = false.obs;
@@ -91,6 +93,11 @@ class RegisterController extends GetxController {
     ).showModal(context);
   }
 
+  resendOTP() {
+    otpResendCount.value++;
+    countdown.value = 60 * otpResendCount.value;
+  }
+
   void registerUser() async {
     // check if user has added data
     if (emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
@@ -130,15 +137,75 @@ class RegisterController extends GetxController {
       currentPageIndex.value == 0;
       return;
     }
-    pageController.animateToPage(
-      currentPageIndex.value + 1,
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeIn,
-    );
 
     // Log every text editing controller in formate like - Feild Name - controller value
 
     if (0 == currentPageIndex.value) {
+      if (nameController.text.isEmpty) {
+        showErrorToast(
+          context,
+          "Name can't be empty",
+          animationCurve: Curves.fastLinearToSlowEaseIn,
+        );
+        return;
+      }
+      if (registerationNoController.text.isEmpty) {
+        showErrorToast(
+          context,
+          "Registeration No can't be empty",
+          animationCurve: Curves.fastLinearToSlowEaseIn,
+        );
+        return;
+      }
+      if (emailController.text.isEmpty) {
+        showErrorToast(
+          context,
+          "Email can't be empty",
+          animationCurve: Curves.fastLinearToSlowEaseIn,
+        );
+        return;
+      }
+      if (!GetUtils.isEmail(emailController.text)) {
+        showErrorToast(
+          context,
+          "Email is not valid",
+          animationCurve: Curves.fastLinearToSlowEaseIn,
+        );
+        return;
+      }
+      // email check for domain vitbhopal.ac.in
+      if (emailController.text.split("@").last != "vitbhopal.ac.in") {
+        showErrorToast(
+          context,
+          "Email should be of vitbhopal.ac.in domain only",
+        );
+        return;
+      }
+      if (passwordController.text.isEmpty) {
+        showErrorToast(
+          context,
+          "Password can't be empty",
+          animationCurve: Curves.fastLinearToSlowEaseIn,
+        );
+        return;
+      }
+      if (cpasswordController.text.isEmpty) {
+        showErrorToast(
+          context,
+          "Confirm Password can't be empty",
+          animationCurve: Curves.fastLinearToSlowEaseIn,
+        );
+        return;
+      }
+      if (passwordController.text != cpasswordController.text) {
+        showErrorToast(
+          context,
+          "Password and Confirm Password should be same",
+          animationCurve: Curves.fastLinearToSlowEaseIn,
+        );
+        return;
+      }
+
       log("Name - ${nameController.text}");
       log("Registeration No - ${registerationNoController.text}");
       log("Email - ${emailController.text}");
@@ -147,6 +214,60 @@ class RegisterController extends GetxController {
       log("Phone - ${phoneController.text}");
     }
     if (1 == currentPageIndex.value) {
+      if (phoneController.text.isEmpty) {
+        showErrorToast(
+          context,
+          "Phone can't be empty",
+          animationCurve: Curves.fastLinearToSlowEaseIn,
+        );
+        return;
+      }
+
+      if (!GetUtils.isPhoneNumber(phoneController.text)) {
+        showErrorToast(
+          context,
+          "Phone is not valid",
+          animationCurve: Curves.fastLinearToSlowEaseIn,
+        );
+        return;
+      }
+
+      if (gender.value.isEmpty) {
+        showErrorToast(
+          context,
+          "Gender can't be empty",
+          animationCurve: Curves.fastLinearToSlowEaseIn,
+        );
+        return;
+      }
+
+      if (Dob.value == "") {
+        showErrorToast(
+          context,
+          "Dob can't be empty",
+          animationCurve: Curves.fastLinearToSlowEaseIn,
+        );
+        return;
+      }
+
+      if ((DateTime.now().year - int.parse(Dob.value.split('-').last) < 16)) {
+        showErrorToast(
+          context,
+          "Age should be less than 16",
+          animationCurve: Curves.fastLinearToSlowEaseIn,
+        );
+        return;
+      }
+
+      if (joiningYear.value.isEmpty) {
+        showErrorToast(
+          context,
+          "Joining Year can't be empty",
+          animationCurve: Curves.fastLinearToSlowEaseIn,
+        );
+        return;
+      }
+
       log("Name - ${nameController.text}");
       log("Registeration No - ${registerationNoController.text}");
       log("Email - ${emailController.text}");
@@ -159,14 +280,30 @@ class RegisterController extends GetxController {
       log("Dob - ${Dob.value.toString()}");
 
       registerUser();
+
+      otpResendCount.value++;
+      countdown.value = 30 * otpResendCount.value;
     }
     if (2 == currentPageIndex.value) {
+      if (otpController.text.isEmpty) {
+        showErrorToast(
+          context,
+          "OTP can't be empty",
+          animationCurve: Curves.fastLinearToSlowEaseIn,
+        );
+        return;
+      }
+
       log("Otp - ${otpController.text}");
       log("Is Verified - ${isVerified.value}");
       log("Can Resend - ${canResend.value}");
       log("Page Index - ${currentPageIndex.value}");
     }
-
+    pageController.animateToPage(
+      currentPageIndex.value + 1,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeIn,
+    );
     currentPageIndex.value = currentPageIndex.value + 1;
   }
 
