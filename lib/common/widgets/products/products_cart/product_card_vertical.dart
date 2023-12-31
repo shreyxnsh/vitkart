90fac52fb25f86c3677d55ed8bfd4692b43caace
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-31 17:25:11 +0530

    product data fetched in ProductCartVeritical

--- lib/common/widgets/products/products_cart/product_card_vertical.dart
+++ lib/common/widgets/products/products_cart/product_card_vertical.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:vitkart/common/widgets/custom_shapes/containers/t_rounded_containers.dart';
 import 'package:vitkart/common/widgets/icons/t_circular_icon.dart';
 import 'package:vitkart/common/widgets/images/t_rounded_image.dart';
 import 'package:vitkart/common/widgets/text/product_price_text.dart';
 import 'package:vitkart/common/widgets/text/product_title_text.dart';
+import 'package:vitkart/common/widgets/text/t_brand_title_with_verifiedicon.dart';
 import 'package:vitkart/features/shop/screens/product_details/product_detail.dart';
-import 'package:vitkart/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:vitkart/utils/constants/colors.dart';
 import 'package:vitkart/utils/constants/image_strings.dart';
 import 'package:vitkart/utils/constants/sizes.dart';
 import 'package:vitkart/utils/helpers/helper_functions.dart';
 
 class TProductCardVertical extends StatelessWidget {
-  const TProductCardVertical({super.key});
+  final ProductData product;
+
+  const TProductCardVertical({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -27,63 +31,46 @@ class TProductCardVertical extends StatelessWidget {
         padding: const EdgeInsets.all(0),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(TSizes.productImageRadius),
-          // boxShadow: [
-          //   TShadowsStyle.verticalProductShadow,
-          // ],
           color: dark ? TColors.lightDarkBackground : TColors.light,
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            /// Thumbnail
             Padding(
               padding: const EdgeInsets.all(TSizes.xs),
               child: TRoundedContainer(
-                height: TSizes.productItemHeight ,
+                
+                height: TSizes.productItemHeight,
                 margin: const EdgeInsets.only(top: 4),
                 padding: const EdgeInsets.all(TSizes.xs),
-                backgroundColor: dark ? TColors.lightDarkBackground : TColors.light,
+                backgroundColor:
+                    dark ? TColors.lightDarkBackground : TColors.light,
                 child: Stack(
                   children: [
                     Center(
-                      child:  TRoundedImage(
-                        applyImageRadius: true,
+                      child: Image.network(
+                        product.productImage,
                         width: 200,
                         height: 350,
-                        imageUrl: TImages.productImage1,
+                        fit: BoxFit.cover,
+                        loadingBuilder: (BuildContext context, Widget child,
+                            ImageChunkEvent? loadingProgress) {
+                          if (loadingProgress == null) {
+                            return child;
+                          } else {
+                            // You can return a placeholder or a loading indicator here
+                            return CircularProgressIndicator();
+                          }
+                        },
+                        errorBuilder: (BuildContext context, Object error,
+                            StackTrace? stackTrace) {
+                          // You can return an error placeholder or handle errors here
+                          return Icon(
+                              Icons.error); // Example: Show an error icon
+                        },
                       ),
                     ),
-
-                    /// sale tag
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Positioned(
-                        top: 12,
-                        child: TRoundedContainer(
-                          radius: TSizes.sm,
-                          backgroundColor: TColors.secondary.withOpacity(0.8),
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: TSizes.sm, vertical: TSizes.xs),
-                          child: Text(
-                            '25%',
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelLarge!
-                                .apply(color: TColors.black),
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    /// Favourite
-                    const Positioned(
-                      top: 2,
-                      right: 2,
-                      child: TCircularIcon(
-                        icon: Iconsax.heart5,
-                        color: Colors.red,
-                      ),
-                    )
+                    // ... Other parts of the Stack
                   ],
                 ),
               ),
@@ -91,35 +78,32 @@ class TProductCardVertical extends StatelessWidget {
             const SizedBox(
               height: TSizes.spaceBtwItems / 2,
             ),
-
-            ///  Details
             Padding(
               padding: const EdgeInsets.only(left: 8),
               child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TProductTitleText(
-                      title: "Green Nike Air Shoes",
-                      smallSize: true,
-                    ),
-                    SizedBox(
-                      height: TSizes.spaceBtwItems / 2,
-                    ),
-                    TBrandTitleWithVerifiedIcon(
-                      title: 'Shreyansh Jain',
-                    ),
-                  ],
-                ),
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TProductTitleText(
+                    title: product.productName,
+                    smallSize: true,
+                  ),
+                  SizedBox(
+                    height: TSizes.spaceBtwItems / 2,
+                  ),
+                  TBrandTitleWithVerifiedIcon(
+                    title: product.sellerName,
+                  ),
+                ],
+              ),
             ),
-            
             const Spacer(),
             Padding(
               padding: const EdgeInsets.only(left: TSizes.sm),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const TProductPriceText(
-                    price: "100",
+                  TProductPriceText(
+                    price: product.productPrice.toString(),
                   ),
                   Container(
                     decoration: const BoxDecoration(
@@ -134,7 +118,7 @@ class TProductCardVertical extends StatelessWidget {
                       height: TSizes.iconLg * 1.2,
                       child: Center(
                         child: Icon(
-                          Iconsax.add,
+                          Icons.add,
                           color: TColors.white,
                         ),
                       ),
@@ -146,6 +130,47 @@ class TProductCardVertical extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class ProductData {
+  final String id;
+  final String productName;
+  final String productDesc;
+  final String productImage;
+  final List<String> productCategory;
+  final double productPrice;
+  final int productStock;
+  final bool isPopular;
+  final String sellerName;
+  final String productStatus;
+
+  ProductData({
+    required this.id,
+    required this.productName,
+    required this.productDesc,
+    required this.productImage,
+    required this.productCategory,
+    required this.productPrice,
+    required this.productStock,
+    required this.isPopular,
+    required this.sellerName,
+    required this.productStatus,
+  });
+
+  factory ProductData.fromJson(Map<String, dynamic> json) {
+    return ProductData(
+      id: json['_id'],
+      productName: json['productName'],
+      productDesc: json['productDesc'],
+      productImage: json['productImage'],
+      productCategory: List<String>.from(json['productCategory']),
+      productPrice: json['productPrice'].toDouble(),
+      productStock: json['productStock'],
+      isPopular: json['isPopular'],
+      sellerName: json['sellerName'],
+      productStatus: json['productStatus'],
     );
   }
 }
