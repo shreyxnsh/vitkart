6d993ee6665ef142af4a87a6e7980ee1cfe02365
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-31 17:24:50 +0530

    tech products fetched

--- lib/utils/config/config.dart
+++ lib/utils/config/config.dart
@@ -3,3 +3,9 @@ const url = 'http://192.168.56.1:3001/api/v1';
 const registrationUrl = '$url/user/createNewAccount';
 const loginUrl = '$url/user/signIn';
 const otpVerificationUrl = '$url/emailVerification/verifyEmail';
+
+
+const techProductsUrl = '$url/product/getproduct?category=tech';
+const getAllProductUrl = '$url/product/getproduct';
+
+
