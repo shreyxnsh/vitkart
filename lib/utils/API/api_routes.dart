5c69539c007cdedaa33311f0f655bf48a8ae084d
shreyxnsh <shreyanshatwork@gmail.com>
2024-01-06 12:55:40 +0530

    YourProducts get request fetched

--- lib/utils/API/api_routes.dart
+++ lib/utils/API/api_routes.dart
@@ -1,7 +1,12 @@
 // 192.168.56.1
+
+
 const String domain = 'http://192.168.56.1:3001';
 // const String domain =
 //     'https://172f-2405-201-682b-707a-b365-6158-77dd-e5df.ngrok-free.app';
+
+
+
 const url = '$domain/api/v1';
 const registrationUrl = '$url/user/createNewAccount';
 const loginUrl = '$url/user/signIn';
@@ -12,4 +17,5 @@ const cycleProductsUrl = '$url/product/getproduct?category=cycle';
 const mattressProductsUrl = '$url/product/getproduct?category=mattress';
 const getAllProductUrl = '$url/product/getproduct';
 const getUserProducts = '$url/product/getproduct?seller=';
+
 const createProductUrl = "$url/product/createProduct?token=";
