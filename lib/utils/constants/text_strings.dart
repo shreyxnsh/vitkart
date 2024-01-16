176e3a6fcc6bdbb2ffff0e77f5312247380fcfde
shreyxnsh <shreyanshatwork@gmail.com>
2024-01-16 15:04:58 +0530

    appname in text strings

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -5,7 +5,7 @@ class TTexts {
   static const String skip = "Skip";
   static const String done = "Done";
   static const String submit = "Submit";
-  static const String appName = "T-Store";
+  static const String appName = "VITKart";
   static const String tContinue = "Continue";
 
   // -- OnBoarding Texts
