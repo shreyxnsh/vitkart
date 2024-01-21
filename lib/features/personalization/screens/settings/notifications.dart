97a614414c442c19d9745107cd2818fd01740666
shreyxnsh <shreyanshatwork@gmail.com>
2024-01-21 20:08:14 +0530

    TermsAndConditions completed

---
+++ lib/features/personalization/screens/settings/notifications.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:vitkart/common/widgets/appbar/appbar.dart';
+import 'package:vitkart/common/widgets/text/section_heading.dart';
+import 'package:vitkart/features/events/screens/widgets/eventTicketVertical.dart';
+import 'package:vitkart/utils/constants/colors.dart';
+import 'package:vitkart/utils/constants/sizes.dart';
+
+class NotificationScreen extends StatelessWidget {
+  const NotificationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TAppBar(
+        title: Text(
+          "Notifications",
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+      ),
+      body: const SingleChildScrollView(
+        child: Padding(
+            padding: EdgeInsets.all(TSizes.defaultSpace),
+            child: Column(
+              children: [
+                
+              ],
+            )),
+      ),
+    );
+  }
+}
