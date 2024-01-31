cb3c70ab5cb8d4eabe54bf7d18c8b6a0c176947f
Aviral yadav <58760825+yaviral17@users.noreply.github.com>
2024-01-31 22:26:29 +0530

    in app update

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:in_app_update/in_app_update.dart';
 import 'package:vitkart/features/authentication/screens/login/login.dart';
 import 'package:vitkart/navigation_menu.dart';
 import 'package:vitkart/utils/API/userDataService.dart';
 import 'package:vitkart/utils/constants/text_strings.dart';
 import 'package:vitkart/utils/theme/theme.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  Future<void> checkForUpdate() async {
+    print('checking for Update');
+    InAppUpdate.checkForUpdate().then((info) {
+      setState(() {
+        if (info.updateAvailability == UpdateAvailability.updateAvailable) {
+          print('update available');
+          update();
+        }
+      });
+    }).catchError((e) {
+      print(e.toString());
+    });
+  }
+
+  void update() async {
+    print('Updating');
+    await InAppUpdate.startFlexibleUpdate();
+    InAppUpdate.completeFlexibleUpdate().then((_) {}).catchError((e) {
+      print(e.toString());
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkForUpdate();
+  }
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: TTexts.appName,
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
+      themeMode: ThemeMode.dark,
+      theme: TAppTheme.darkTheme,
       darkTheme: TAppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
       home: UserDataService.isUserLoggednIn()
