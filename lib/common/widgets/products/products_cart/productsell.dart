7e475b8f034a37bf00778e9ef533591ee9e323e4
shreyansh-bwi <shreyansh.jain@buildwithinnovation.com>
2024-03-05 15:34:16 +0530

    product details dynamic data

---
+++ lib/common/widgets/products/products_cart/productsell.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import 'package:vitkart/features/shop/screens/store/create_product.dart';
+import 'package:vitkart/features/shop/screens/store/sell.dart';
+import 'package:vitkart/navigation_menu.dart';
+import 'package:vitkart/utils/constants/colors.dart';
+import 'package:vitkart/utils/constants/sizes.dart';
+import 'package:vitkart/utils/helpers/helper_functions.dart';
+
+class SellSuccessScreen extends StatefulWidget {
+  const SellSuccessScreen({super.key});
+
+  @override
+  State<SellSuccessScreen> createState() => _SuccessScreenState();
+}
+
+class _SuccessScreenState extends State<SellSuccessScreen> {
+  bool animateIt = false;
+  bool fadeIt = false;
+  final animationDuratino = const Duration(milliseconds: 600);
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 1200), () {
+      setState(() {
+        animateIt = true;
+      });
+    });
+
+    Future.delayed(Duration(milliseconds: 2000), () {
+      setState(() {
+        fadeIt = true;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Scaffold(
+      backgroundColor: dark ? TColors.dark : TColors.light,
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      // floatingActionButton: AnimatedOpacity(
+      //   duration: animationDuratino,
+      //   opacity: fadeIt ? 1.0 : 0.0,
+      //   child: Container(
+      //     margin: EdgeInsets.all(TSizes.spaceBtwItems),
+      //     height: TSizes.displayHeight(context) * (54 / 840),
+      //     child: Row(
+      //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //       mainAxisSize: MainAxisSize.max,
+      //       children: [
+      //         Expanded(
+      //           child: ElevatedButton(
+      //             onPressed: () {
+      //               Get.to(() => CreateProductScreen());
+      //             },
+      //             child: const Center(child: Text('Add More')),
+      //           ),
+      //         ),
+      //         const SizedBox(
+      //           width: TSizes.spaceBtwItems,
+      //         ),
+      //         Expanded(
+      //           child: ElevatedButton(
+      //             onPressed: () {
+      //               Get.to(() => NavigationMenu());
+      //             },
+      //             child: const Center(child: Text('View Product')),
+      //           ),
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      body: WillPopScope(
+        onWillPop: () async {
+          Get.offAll(const NavigationMenu());
+          return true;
+        },
+        child: SizedBox(
+          height: TSizes.displayHeight(context),
+          width: TSizes.displayWidth(context),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Lottie.asset(
+                    'assets/lottie/success_animation.json',
+                    repeat: false,
+                    width: TSizes.displayWidth(context) * 0.8,
+                    fit: BoxFit.fitWidth,
+                    // animate: animateIt,
+                  ),
+                  SizedBox(
+                    width: TSizes.displayWidth(context) * 0.8,
+                    child: AnimatedOpacity(
+                      duration: animationDuratino,
+                      opacity: fadeIt ? 1 : 0,
+                      child: Center(
+                        child: Text(
+                          'Product Bought Successfully',
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleMedium!
+                              .copyWith(
+                                fontSize:
+                                    TSizes.displayWidth(context) * (24 / 420),
+                                color: TColors.primary,
+                              ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Lottie.asset(
+                'assets/lottie/success_bg.json',
+                repeat: false,
+                fit: BoxFit.fitHeight,
+                height: TSizes.displayHeight(context),
+                animate: animateIt,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,72 +1,236 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:readmore/readmore.dart';
+import 'package:vitkart/common/widgets/appbar/appbar.dart';
+import 'package:vitkart/common/widgets/custom_shapes/containers/t_rounded_containers.dart';
+import 'package:vitkart/common/widgets/icons/t_circular_icon.dart';
+import 'package:vitkart/common/widgets/images/t_rounded_image.dart';
+import 'package:vitkart/common/widgets/products/products_cart/product_card_vertical.dart';
+import 'package:vitkart/common/widgets/text/product_price_text.dart';
+import 'package:vitkart/common/widgets/text/product_title_text.dart';
 import 'package:vitkart/common/widgets/text/section_heading.dart';
 import 'package:vitkart/features/shop/screens/product_details/widgets/bottom_add_to_cart.dart';
 import 'package:vitkart/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
 import 'package:vitkart/features/shop/screens/product_details/widgets/product_meta_data.dart';
+import 'package:vitkart/features/shop/screens/product_details/widgets/rating_share_button.dart';
+import 'package:vitkart/utils/constants/colors.dart';
+import 'package:vitkart/utils/constants/image_strings.dart';
 import 'package:vitkart/utils/constants/sizes.dart';
+import 'package:vitkart/utils/helpers/helper_functions.dart';
+class ProductDetailScreen extends StatefulWidget {
+  final ProductData product;
 
-class ProductDetailScreen extends StatelessWidget {
-  const ProductDetailScreen({super.key});
+  const ProductDetailScreen({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
 
   @override
+  State<ProductDetailScreen> createState() => _ProductDetailScreenState();
+}
+
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final dark = THelperFunctions.isDarkMode(context);
+    return Scaffold(
       bottomNavigationBar: TBootomAddToCart(),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            // 1 Product Image Slider
-            TProductImageSlider(),
-
-            // 2 Product Details
+            // Product Image Slider
+            TCurverEdgesWidget(
+              child: Container(
+                color: dark ? TColors.darkerGrey : TColors.light,
+                child: Stack(
+                  children: [
+                    // Main Large Image
+                    SizedBox(
+  height: 420,
+  child: Padding(
+    padding: EdgeInsets.all(TSizes.productImageRadius),
+    child: Center(
+      child: CachedNetworkImage(
+        imageUrl: widget.product.productImage,
+        placeholder: (context, url) => CircularProgressIndicator(),
+        errorWidget: (context, url, error) => Icon(Icons.error),
+      ),
+    ),
+  ),
+),
+                    // Image Slider
+                    // Positioned(
+                    //   right: 0,
+                    //   bottom: 30,
+                    //   left: TSizes.defaultSpace,
+                    //   child: Column(
+                    //     children: [
+                    //       SizedBox(
+                    //         height: 80,
+                    //         child: ListView.separated(
+                    //           shrinkWrap: true,
+                    //           scrollDirection: Axis.horizontal,
+                    //           physics: const AlwaysScrollableScrollPhysics(),
+                    //           itemCount: widget.product.productImage.length,
+                    //           separatorBuilder: (_, __) =>
+                    //               const SizedBox(width: TSizes.spaceBtwItems),
+                    //           itemBuilder: (_, index) => TRoundedImage(
+                    //             width: 80,
+                    //             backgroundColor:
+                    //                 dark ? TColors.dark : TColors.white,
+                    //             border: Border.all(
+                    //               color: TColors.primary,
+                    //             ),
+                    //             padding: const EdgeInsets.all(TSizes.sm),
+                    //             imageUrl: widget.product.productImage[index],
+                    //           ),
+                    //         ),
+                    //       ),
+                    //       const SizedBox(
+                    //         height: 10,
+                    //       ),
+                    //     ],
+                    //   ),
+                    // ),
+                    // Appbar Icon
+                    const TAppBar(
+                      showBackArrow: true,
+                      actions: [
+                        TCircularIcon(
+                          icon: Iconsax.heart5,
+                          color: Colors.red,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            // Product Details
             Padding(
               padding: EdgeInsets.only(
-                
                 right: TSizes.defaultSpace,
                 left: TSizes.defaultSpace,
                 bottom: TSizes.defaultSpace,
               ),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   // Rating and Share Button
-    
-
                   /// - Price,Title ,Stock & Brand
-                  TProductMetaData(),
-                  // const SizedBox(
-                  //   height: TSizes.spaceBtwItems,
-                  // ),
-
-                  // /// -- Attributes
-                  // const TProductAttributes(),
-                  SizedBox(
+                  TRoundedContainer(
+                    padding: const EdgeInsets.all(TSizes.md),
+                    backgroundColor: dark
+                        ? TColors.lightDarkBackground
+                        : TColors.light,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        /// Price & Sale Price
+                        Row(
+                          children: [
+                            /// Sale tag
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                TRoundedContainer(
+                                  radius: TSizes.sm,
+                                  backgroundColor:
+                                      TColors.secondary.withOpacity(0.8),
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: TSizes.sm, vertical: TSizes.xs),
+                                  child: Text(
+                                    "25 %",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .labelLarge!
+                                        .apply(color: TColors.black),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: TSizes.spaceBtwItems,
+                                ),
+                                // Price
+                                Text(
+                                  "\$${widget.product.productPrice}",
+                                  style: Theme.of(context).textTheme.titleSmall!.apply(
+                                        decoration: TextDecoration.lineThrough,
+                                      ),
+                                ),
+                                const SizedBox(
+                                  width: TSizes.spaceBtwItems,
+                                ),
+                                TProductPriceText(
+                                  price: "${widget.product.productPrice}",
+                                  isLarge: true,
+                                ),
+                              ],
+                            ),
+                            const Spacer(),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.share,
+                                size: TSizes.iconMd,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: TSizes.spaceBtwItems / 1.5,
+                        ),
+                        /// Titile
+                        TProductTitleText(
+                          title: widget.product.productName,
+                          smallSize: false,
+                        ),
+                        const SizedBox(
+                          height: TSizes.spaceBtwItems / 1.5,
+                        ),
+                        /// Stock Status
+                        Row(
+                          children: [
+                            const TProductTitleText(
+                              title: "Quantity :",
+                              smallSize: true,
+                            ),
+                            const SizedBox(
+                              width: TSizes.spaceBtwItems,
+                            ),
+                            Text(
+                              "${widget.product.productStock}",
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: TSizes.spaceBtwItems / 1.5,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
                     height: TSizes.spaceBtwItems,
                   ),
-
-                
-
-                  /// - Descriptioon
-                  TSectionHeading(title: "Description" , showActionButton: false,),
+                  /// - Description
+                  TSectionHeading(title: "Description", showActionButton: false,),
                   SizedBox(height: TSizes.spaceBtwItems),
                   ReadMoreText(
-                    "Introducing our latest innovation: The QuantumX Pro Series Smart Speaker. Elevate your audio experience to new heights with cutting-edge technology and sleek design. Immerse yourself in crystal-clear sound quality that resonates in every corner of your space. The QuantumX Pro is equipped with state-of-the-art audio engineering, delivering deep bass, crisp highs, and a rich midrange. Whether you're hosting a party, working from home, or simply enjoying your favorite tunes, this smart speaker ensures an unparalleled audio journey.\n With seamless Bluetooth connectivity, effortlessly pair your devices and enjoy wireless freedom. The QuantumX Pro also boasts a built-in voice assistant, allowing you to control your music, check the weather, or set reminders with just the sound of your voice.\n Designed with both aesthetics and functionality in mind, the sleek and modern exterior of the QuantumX Pro Series Smart Speaker seamlessly blends into any environment. Its compact size makes it perfect for on-the-go adventures, yet its powerful performance ensures it's the life of the party.\nUpgrade your audio experience with the QuantumX Pro Series Smart Speaker – where innovation meets sound. Elevate your senses and redefine how you experience music and entertainment.",
+                    "${widget.product.productDesc}",
                     trimLines: 4,
                     trimMode: TrimMode.Line,
                     trimCollapsedText: " Show more",
                     trimExpandedText: "\n\nShow less",
-                    moreStyle:
-                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
-                    lessStyle:
-                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                    moreStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                    lessStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
                   ),
 
+                  
                   /// - Reviews
-                  // const Divider(),
                   SizedBox(
                     height: TSizes.spaceBtwItems,
                   ),
-                  
                   SizedBox(height: TSizes.spaceBtwSections),
                 ],
               ),
